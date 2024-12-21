62875da26dd58a5935f2f7a34ce008b117cf20a3
TRON-SV <jerson_trn_232@hotmail.com>
2024-12-21 11:11:02 -0600

    First project commit; Init project structure

---
+++ src/main/java/com/backend/puntoxpress/PuntoxpressApplication.java
@@ -0,0 +1,13 @@
+package com.backend.puntoxpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoxpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoxpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/puntoxpress/PuntoxpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.puntoxpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoxpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
